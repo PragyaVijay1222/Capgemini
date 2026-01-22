9d8447c714dd5978609c6b735b9a6600f34450ce
Pragya Vijay <118822753+PragyaVijay1222@users.noreply.github.com>
2026-01-22 22:24:11 +0530

    Create Celsius.java

---
+++ Core Java Fundamentals/Hello World Programs/Celsius.java
@@ -0,0 +1,9 @@
+
+public class Celsius {
+ public static void main(String[] args) {
+	 int cel = 100;
+	 int fahren = cel * (9/5) +  32;
+	 
+	 System.out.println("Fahrenheit value is " + fahren);
+ }
+}
