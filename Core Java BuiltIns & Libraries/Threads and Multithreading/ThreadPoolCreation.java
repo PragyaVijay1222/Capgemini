32af824ed76cc9cd400107e7d264082c0056010a
Pragya Vijay <118822753+PragyaVijay1222@users.noreply.github.com>
2026-01-22 15:51:18 +0530

    Create ThreadPoolCreation.java

---
+++ Core Java BuiltIns & Libraries/Threads and Multithreading/ThreadPoolCreation.java
@@ -0,0 +1,40 @@
+// 22/01/26
+// Thread pool
+
+package MultiThreading;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolCreation {
+	  public static void main(String[] args) {
+
+	        // Create thread pool of size 3
+	        ExecutorService service = Executors.newFixedThreadPool(3);
+
+	        // Submit 6 tasks
+	        for (int i = 1; i <= 6; i++) {
+	            service.execute(new Task(i));
+	        }
+
+	        // Shutdown pool
+	        service.shutdown();
+	  }
+}
+
+class Task implements Runnable {
+
+    int taskId;
+
+    Task(int taskId) {
+        this.taskId = taskId;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(
+            "Task " + taskId +
+            " executed by " +
+            Thread.currentThread().getName()
+        );
+    }
+}
