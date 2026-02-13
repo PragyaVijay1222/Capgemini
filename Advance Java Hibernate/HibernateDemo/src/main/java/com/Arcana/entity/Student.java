185caa7b692a647eca70fcb0ed0db0865da22f12
Pragya Vijay <118822753+PragyaVijay1222@users.noreply.github.com>
2026-02-13 19:46:50 +0530

    Add files via upload

---
+++ Advance Java Hibernate/HibernateDemo/src/main/java/com/Arcana/entity/Student.java
@@ -0,0 +1,56 @@
+package com.Arcana.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "student_name")
+    private String name;
+
+    @Column(name = "student_age")
+    private int age;
+	
+	public Student() {
+		
+	}
+	public Student( String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	public String getName() {
+		return this.name;
+	}
+	public int getAge() {
+		return this.age;
+	}
+	
+	@Override
+	public String toString() {
+		return ("ID: "+this.id+"Name: "+this.name+" Age: "+this.age);
+	}
+	
+}
